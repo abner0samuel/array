23a6b7fe3e988c58b5564827e51233a7b05e3c48
abner0samuel <abaretabner@gmail.com>
2024-04-25 17:48:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const colors =["red","blue","green","yellow","orange"]
+const [red = lightred,yellow = lightyellow,blue=20, ...color]= colors
+console.log(red);
+console.log(yellow);
+console.log(blue);
+console.log(color);
